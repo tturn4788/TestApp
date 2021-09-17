f50dac7ae88ce65a07e6a0c91527bccb60d5f8a9
tturn4788 <90854850+tturn4788@users.noreply.github.com>
2021-09-17 12:30:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-import numpy as np
-from PIL import Image
 import streamlit as st
-from Helper import load_data, summary_poster
+import pandas as pd
+import numpy as np
 
-stats_df = load_data("./data/SampleforWork4.csv")
-color_map_df = load_data("./data/color_map_df.csv")
+st.title('Proof of Concept Data App')
 
-st.set_page_config(page_title="Proof of Concept for Tables of Data", 
-                   page_icon=":notes:", 
-                   layout='wide')
+DATE_COLUMN = 'Date'
+DATA_URL = ('./Data/SampleforWork4.csv')
 
-#--------------------------------- ---------------------------------  ---------------------------------
-#--------------------------------- SETTING UP THE APP
-#--------------------------------- ---------------------------------  ---------------------------------
-title_image = Image.open("./plots/AppTitle.jpg")
-st.image(title_image)
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-st.markdown("A Data Geek's take on the question ***'How have music tastes changed through the years?'***")
-st.markdown("This app is meant as a proof of concept to demonstrate the utility of web-based interfaces for Excel files")
-#---------------------------------------------------------------#
-# SELECT ARTIST AND SETUP DATA
-#---------------------------------------------------------------#
-sorted_site = stats_df.groupby('Site')['search_query'].count().sort_values(ascending=False).index
+data_load_state = st.text('Loading data...')
+data = load_data(190)
+data_load_state.text("Done! (using st.cache)")
 
-st.markdown("### **Select Site:**")
-select_site = []
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-select_site.append(st.selectbox('', sorted_site))
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-#Filter df based on selection
-site_df = stats_df[stats_df['search_site'].isin(select_site)]
+# Some number in the range 0-11
+site_to_filter = st.slider('Site', 0, 11, 17)
+filtered_data = data[data['Site'] == site_to_filter]
 
-major_cluster = site_df.groupby('Date')['search_query'].count().sort_values(ascending = False).index[0]
+#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+#st.(filtered_data)
+
+chart_data = filtered_data(
+     columns=['', 'b', 'c'])
+
+st.line_chart(x = filtered_data['Date'], y=filtered_data['System'])ex[0]
