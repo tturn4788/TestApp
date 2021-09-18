2b8310ef641fcca9c673820b15bf3ea039332a0b
tturn4788 <90854850+tturn4788@users.noreply.github.com>
2021-09-18 10:54:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ df = pd.DataFrame({
 import plotly.express as px
 
 fig = px.bar(df, x="Fruit", y="Number Eaten", color="Contestant", barmode="group")
-st.show(fig)
+st.draw(fig)
 
 
 # Graph Objects
