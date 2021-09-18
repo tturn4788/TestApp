9ffc82a4d4b90592b53edf3b5dcf6b8217392100
tturn4788 <90854850+tturn4788@users.noreply.github.com>
2021-09-18 11:54:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ hist_values = hist_values.set_index('Date', inplace=True)
 'hist_values.groupby("Site")["System"].plot(legend=True)'
 
 
-SITES = data.columns.unique()
+SITES = data.Site.unique()
 SITES_SELECTED = st.multiselect('Select countries', SITES)
 # Mask to filter dataframe
-mask_site = data.columns.isin(SITES_SELECTED)
+mask_site = data['Site'].isin(SITES_SELECTED)
 
-data_mask = data[data[mask_site]]
+data_mask = data[mask_site]
 
 #replace if the above works
 #all_columns_names= data.columns.tolist()
