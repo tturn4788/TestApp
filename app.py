4e46f94d6b12b029eee59d48c4e4222593173a2e
tturn4788 <90854850+tturn4788@users.noreply.github.com>
2021-09-17 20:42:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,8 @@ basicc_chart = basically_chart[['Site','Date','System']].plot(x='Date', y='Syste
 
 st.markdown('Second attempt at filtered line chart')
 
-filter_data = basicc_chart[['Date','Site','System']]
+'Heres the problem.'
+filter_data = basicc_chart[['Date','Site','System']].set_index('Date').groupby(['Site'])
 st.line_chart(filter_data)
 
 st.markdown("Third time's a charm? Nah")
