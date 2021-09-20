85862ee5af52896d89e49e590b0f0fa86c4391d7
tturn4788 <90854850+tturn4788@users.noreply.github.com>
2021-09-20 12:08:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if st.checkbox('Show raw data'):
 fig = go.Figure()
 
 
-fig.add_trace((x = data.index, y = data.System, mode = 'lines'))
+fig.add_trace(x = data.index, y = data.System, mode = 'lines')
 st.line_chart(fig, use_container_width=True)
 
 col1, col2 = st.beta_columns(2)
